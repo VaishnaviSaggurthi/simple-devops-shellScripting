4c522754abf8e1d7a675821ce9a3a62e63f0993c
VaishnaviSaggurthi <vaishnavisaggurthi@gmail.com>
2026-02-08 11:18:31 +0530

    This is the code for calculator for adding,subtracting,multiplying and dividing two numbers

--- calculator.sh
+++ calculator.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+
+################
+# Author: Vaishnavi Saggurthi
+# Date: 08-02-2026
+# Version: V1
+###############
+
+# This is a basic calculator code which adds,subtracts,multiplies and divides two numbers
+
 a=10
 b=20
 x=$((a+b))
 y=$((b-a))
+z=$((a*b))
+i=$((b/a))
 echo $x
 echo $y
+echo $z
+echo $i
